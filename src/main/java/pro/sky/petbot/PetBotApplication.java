9762e6b0eb1f624f88f39933678af471520660fc
HavierVislon <haviervilson@gmail.com>
2023-06-02 19:08:52 +0300

    release

---
+++ src/main/java/pro/sky/petbot/PetBotApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.petbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetBotApplication.class, args);
+    }
+
+}
